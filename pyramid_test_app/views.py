58c9d599ecd2f95cd2e78c5f2c558426a4885a83
Bert JW Regeer <bertjw@regeer.org>
2014-04-28 19:22:56 -0600

    Test pyramid_pluggable_session

--- pyramid_test_app/views.py
+++ pyramid_test_app/views.py
@@ -3,4 +3,12 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'pyramid_test_app'}
+    if 'here' in request.session:
+        here = True
+        request.session['here_count'] += 1
+    else:
+        here = False
+        request.session['here'] = True
+        request.session['here_count'] = 0
+
+    return {'project': 'pyramid_test_app', 'here': here, 'here_count': request.session['here_count']}
